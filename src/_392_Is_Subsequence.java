f29ad5666d3bf846754b3b2eeb058f09cee9265c
Dmitry Zolotyh <zolotyh.dk@yandex.ru>
2024-12-08 10:14:53 +0300

    Solve "Is Subsequence" problem

---
+++ src/_392_Is_Subsequence.java
@@ -0,0 +1,30 @@
+public class _392_Is_Subsequence {
+    public static void main(String[] args) {
+        _392_Is_Subsequence solution = new _392_Is_Subsequence();
+        String s1 = "abc";
+        String t1 = "ahbgdc";
+        System.out.println(solution.isSubsequence(s1, t1));
+
+        String s2 = "axc";
+        String t2 = "ahbgdc";
+        System.out.println(solution.isSubsequence(s2, t2));
+    }
+
+    public boolean isSubsequence(String s, String t) {
+        if (s.isEmpty()) {
+            return true;
+        }
+
+        int j = 0;
+
+        for (int i = 0; i < t.length(); i++) {
+            if (s.charAt(j) == t.charAt(i)) {
+                j++;
+            }
+            if (j == s.length()) {
+                return true;
+            }
+        }
+        return false;
+    }
+}
